10f32759368062d3994916e609e6420bb7508f28
Elias <you@example.com>
2024-09-27 20:40:40 -0400

    Updating files

--- OOP/Item.py
+++ OOP/Item.py
@@ -1,22 +1,20 @@
 class Item:
-    def __init__(self, name):
-        print(f"An instance created: {name}")
-           
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
     def calculate_total_price(self, x, y):
         return x * y
 
 
-item1 = Item("IPhone")
-item1.name = "IPhone"
-item1.price = 1200
-item1.quantity = 5
+item1 = Item("IPhone", 1200, 5)
 print(item1.calculate_total_price(item1.price, item1.quantity))
 
-item2 = Item("Laptop")
-item2.name = "IPhone"
-item2.price = 2100
-item2.quantity = 3
+item2 = Item("Laptop", 2100, 3)
 print(item2.calculate_total_price(item2.price, item2.quantity))
+
+
 
 
 
