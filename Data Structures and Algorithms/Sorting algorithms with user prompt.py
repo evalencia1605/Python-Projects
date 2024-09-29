f097c2233b90287f837a8709f1f2d4013319848f
Elias <you@example.com>
2024-09-29 18:47:34 -0400

    Updated files

--- Data Structures and Algorithms/Sorting algorithms with user prompt.py
+++ Data Structures and Algorithms/Sorting algorithms with user prompt.py
@@ -61,7 +61,6 @@ def BubbleSort(arr):
 
 n = len(arr)
 def heapify(arr, n, i):
-      # Find largest among root and children
     largest = i
     l = 2 * i + 1
     r = 2 * i + 2
@@ -72,7 +71,6 @@ def heapify(arr, n, i):
     if r < n and arr[largest] < arr[r]:
           largest = r
   
-      # If root is not largest, swap with largest and continue heapifying
     if largest != i:
           arr[i], arr[largest] = arr[largest], arr[i]
           heapify(arr, n, largest)
