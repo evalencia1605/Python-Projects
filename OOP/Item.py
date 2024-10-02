045e4249d3bd6198dfc726f8b6f1231e74411534
Elias <you@example.com>
2024-10-01 20:40:44 -0400

    Updated files

--- OOP/Item.py
+++ OOP/Item.py
@@ -1,26 +1,22 @@
 class Item:
-    def __init__(self, name, price, quantity):
+    def __init__(self, name: str, price: int, quantity: int):
+        #Run validations
+        assert price >= 0, f"Price {price} is not greater than zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than zero!"
+        
         self.name = name
         self.price = price
         self.quantity = quantity
 
-    def calculate_total_price(self, x, y):
-        return x * y
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
-item1 = Item("IPhone", 1200, 5)
-print(item1.calculate_total_price(item1.price, item1.quantity))
+item1 = Item("IPhone", 1600, 5)
+print(item1.calculate_total_price())
 
 item2 = Item("Laptop", 2100, 3)
-print(item2.calculate_total_price(item2.price, item2.quantity))
-
-print(item1.name)
-print(item1.price)
-print(item1.quantity)
-
-print(item2.name)
-print(item2.price)
-print(item2.quantity)
+print(item2.calculate_total_price())
 
 
 
