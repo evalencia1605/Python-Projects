57ff06f51937688c33c664111c2ffb4aebb5787c
Elias <you@example.com>
2024-10-24 17:25:14 -0400

    Updates files

---
+++ Data Structures and Algorithms/hash table.py
@@ -0,0 +1,21 @@
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+
+class HashTable:
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.size = 0
+        self.table = [None] * capacity
+
+
+    def _hash(self,key):
+        return hash(key) % self.capacity
+    
+
+    def insert(self, key, value):
+        i = self._hash(key)
+
+        
